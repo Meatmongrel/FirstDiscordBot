a90f4fbf8d1cd4af8e5d90f118d73500b5dcc002
Andrew Thiessen <squaids449@gmail.com>
2019-11-02 01:51:39 -0600

    destructured all commands into functions and refactored poke command. added shiny searching functionality to poke command. changed general command input structure, which is now more flexible as opposed to forced constraints upon whats accepted

--- index.js
+++ index.js
@@ -3,6 +3,90 @@ const client = new Client
 const { prefix } = require('./config.json')
 const fetch = require('node-fetch')
 
+const pokeCommand = (message, args) => {
+    if (!args.length) {
+        randomCommand(message)
+    }
+    else if(args[0] === "shiny"){
+        fetchRandomPoke(message, "shiny")
+    }
+    else if(args[0] === 'random'){
+        randomCommand(message, args)
+        
+    }else if(args[1] === "shiny"){
+        fetchPoke(message, args, "shiny")
+    }
+    else{
+        fetchPoke(message, args)
+        
+    }
+}
+
+const randomCommand = (message, args) => {
+    if(!args){
+        fetchRandomPoke(message)
+    }
+    else if(args[1] === "shiny"){
+        if(args[2] <= 5){
+            for(var i=1; i <= args[2]; i++){
+                fetchRandomPoke(message, "shiny")
+            }
+        }else{
+            fetchRandomPoke(message, "shiny")
+        }
+    }
+    else if(args[1] <= 5){
+        if(args[2] === "shiny"){
+            for(var i=1; i <= args[1]; i++){
+                fetchRandomPoke(message, "shiny")
+            }
+        }else{
+            for(var i=1; i <= args[1]; i++){
+                (fetchRandomPoke(message))
+            }
+        }
+    }
+    else{
+        message.channel.send("You can only get 5 random pokemon at a time")
+    }
+}
+
+function fetchPoke(message, args, shiny="default"){
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${args[0].toLowerCase()}`)
+        .then(res => res.json())
+        .then(pokemon => {
+            const poke = new RichEmbed()
+                .setTitle(pokemon.name)
+                .setImage(eval(`pokemon.sprites.front_${shiny}`))
+            message.channel.send(poke)
+            console.log(poke)
+        })
+}
+
+function fetchRandomPoke(message, shiny="default"){
+    const num = Math.floor(Math.random() * 808 + 1)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
+        .then(res => res.json())
+        .then(pokemon => {
+            const poke = new RichEmbed()
+                .setTitle(pokemon.name)
+                .setImage(eval(`pokemon.sprites.front_${shiny}`))
+            message.channel.send(poke)
+        })
+        console.log(num)
+}
+
+const dieCommand = (message, args) => {
+    if(args[0]%1==0 === true){
+        const rand = Math.floor((Math.random()) * args[0] + 1)
+        message.channel.send(`Your roll out of ${args[0]}... ${rand}
+        ${numMessage(args, rand)}`)
+        console.log(args[0])
+    }else if(args[0]%1==0 === false){
+        message.channel.send(`${args[0]} is not a number`)
+    }
+}
 
 function numMessage(args, rand){
     if (rand <= args[0] * 0.25){
@@ -19,74 +103,27 @@ function numMessage(args, rand){
 
 }
 
-function fetchPoke(message, args){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${args[0].toLowerCase()}`)
-        .then(res => res.json())
-        .then(pokemon => {
-            const poke = new RichEmbed()
-                .setTitle(pokemon.name)
-                .setImage(pokemon.sprites.front_default)
-            message.channel.send(poke)
-        })
+const helpCommand = (message) => {
+    const helpMsg = new RichEmbed()
+        .setTitle("The prefix is - and current commands are:")
+        .addField("-d (num)", "Roll a die with (num) sides")
+        .addField("-poke (name/pokedex number) OR (random (num))", "Search a pokemon by name, or get (num) random pokemon, up to 5. You can also add the shiny param to the end of the command to get the shiny version")
+    message.channel.send(helpMsg)
 }
-
-function fetchRandomPoke(message){
-    const num = Math.floor(Math.random() * 808 + 1)
-    fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
-        .then(res => res.json())
-        .then(pokemon => {
-            const poke = new RichEmbed()
-                .setTitle(pokemon.name)
-                .setImage(pokemon.sprites.front_default)
-            message.channel.send(poke)
-        })
-        console.log(num)
-}
-
 client.on('message', message => {
-
-
+    
+    
     const args = message.content.slice(prefix.length).split(' ');
     const command = args.shift().toLowerCase();
-
+    
     if (command === 'd'){
-        if(args[0]%1==0 === true){
-            const rand = Math.floor((Math.random()) * args[0] + 1)
-            message.channel.send(`Your roll out of ${args[0]}... ${rand}
-            ${numMessage(args, rand)}`)
-            console.log(args[0])
-        }else if(args[0]%1==0 === false){
-            message.channel.send(`${args[0]} is not a number`)
-        }
-            
+        dieCommand(message, args)
     }
     else if (command === `poke`) {
-        if (!args.length) {
-            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-        else if(args[0] == 'random'){
-            if(args[1] <= 5){
-                for(var i=1; i <= args[1]; i++){
-                    (fetchRandomPoke(message))
-                }
-            }else if(!args[1]){
-                fetchRandomPoke(message)
-            }
-            else{
-                message.channel.send("You can only get 5 random pokemon at a time")
-            }
-        }
-        else{
-            fetchPoke(message, args)
-
-        }
+        pokeCommand(message, args)
     }
     else if(command === 'help'){
-        const helpMsg = new RichEmbed()
-            .setTitle("The prefix is - and current commands are:")
-            .addField("-d (num)", "Roll a die with (num) sides")
-            .addField("-poke (name) OR (random (num))", "Search a pokemon by name, or get (num) random pokemon, up to 5")
-        message.channel.send(helpMsg)
+        helpCommand(message)
     }
     
 
