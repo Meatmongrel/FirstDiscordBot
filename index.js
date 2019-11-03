7bda544cd7fafa78a8d177871fbebdb59026021a
Andrew <squaids449@gmail.com>
2019-11-02 22:27:59 -0600

    -added prefix command that allows one to change the prefix to their choosing -employed use of node 'file system' to read and write the prefix property -used awaitReaction to confirm change of prefix

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const { RichEmbed, Client } = require('discord.js')
+const fs = require('fs')
 const client = new Client
-const { prefix } = require('./config.json')
 const fetch = require('node-fetch')
+const embedColor = "#5b3687"
+
+fs.readFile('./config.json', (err, data) => {
+    if (err) throw err;
+    return config = JSON.parse(data)
+  });
 
 const pokeCommand = (message, args) => {
     if (!args.length) {
@@ -51,30 +57,44 @@ const randomCommand = (message, args) => {
     }
 }
 
-function fetchPoke(message, args, shiny="default"){
+const fetchPoke = (message, args, shiny="default") => {
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${args[0].toLowerCase()}`)
         .then(res => res.json())
         .then(pokemon => {
             const poke = new RichEmbed()
                 .setTitle(pokemon.name)
+                .setColor(embedColor)
                 .setImage(eval(`pokemon.sprites.front_${shiny}`))
             message.channel.send(poke)
-            console.log(poke)
+            if(shiny === "shiny"){
+                console.log("Getting shiny pokemon")
+
+            }else{
+                console.log("Getting pokemon")
+
+            }
         })
 }
 
-function fetchRandomPoke(message, shiny="default"){
+const fetchRandomPoke = (message, shiny="default") => {
     const num = Math.floor(Math.random() * 808 + 1)
     fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
         .then(res => res.json())
         .then(pokemon => {
             const poke = new RichEmbed()
                 .setTitle(pokemon.name)
+                .setColor(embedColor)
                 .setImage(eval(`pokemon.sprites.front_${shiny}`))
             message.channel.send(poke)
+            if(shiny === "shiny"){
+                console.log("Getting random shiny pokemon")
+
+            }else{
+                console.log("Getting random pokemon")
+
+            }
         })
-        console.log(num)
 }
 
 const dieCommand = (message, args) => {
@@ -82,13 +102,12 @@ const dieCommand = (message, args) => {
         const rand = Math.floor((Math.random()) * args[0] + 1)
         message.channel.send(`Your roll out of ${args[0]}... ${rand}
         ${numMessage(args, rand)}`)
-        console.log(args[0])
     }else if(args[0]%1==0 === false){
         message.channel.send(`${args[0]} is not a number`)
     }
 }
 
-function numMessage(args, rand){
+const numMessage = (args, rand) => {
     if (rand <= args[0] * 0.25){
         return ("Ouch, sucks for you!")
     }else if(rand <= args[0] * 0.5){
@@ -105,25 +124,81 @@ function numMessage(args, rand){
 
 const helpCommand = (message) => {
     const helpMsg = new RichEmbed()
-        .setTitle("The prefix is - and current commands are:")
-        .addField("-d (num)", "Roll a die with (num) sides")
-        .addField("-poke (name/pokedex number) OR (random (num))", "Search a pokemon by name, or get (num) random pokemon, up to 5. You can also add the shiny param to the end of the command to get the shiny version")
+        .setColor(embedColor)
+        .setTitle(`The prefix is ${config.prefix} and current commands are:`)
+        .addField(`${config.prefix}d (num)", "Roll a die with (num) sides`)
+        .addField(`${config.prefix}poke (name/pokedex number) OR (random (num))", "Search a pokemon by name, or get (num) random pokemon, up to 5. You can also add the shiny param to the end of the command to get the shiny version`)
     message.channel.send(helpMsg)
+}
+
+const prefixCommand = (message, args) => {
+    if (!args.length){
+        const prefixMsg = new RichEmbed()
+            .setTitle(`The current prefix is ${config.prefix}`)
+            .setColor(embedColor)
+        message.channel.send(prefixMsg)
+    }else if(args[0] === "set"){
+        if(args[1]){
+            message.channel.send(`Are you sure you want to set the prefix to ${args[1]}`)
+            message.react('👍').then(() => message.react('👎'))
+            const filter = (reaction, user) => { return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id}
+            message.awaitReactions(filter, { max: 1, time: 15000 })
+                .then(collected => {
+                    const reaction = collected.first()
+                    if(reaction.emoji.name === '👍'){
+                        fs.writeFile("config.json", JSON.stringify({"prefix": `${args[1]}`}), (err) => {
+                            if (err) throw err;
+                            console.log('The file has been saved!');
+                        });
+                        fs.readFile('./config.json', (err, data) => {
+                            if (err) throw err;
+                            return config = JSON.parse(data)
+                          });
+                        const prefixSuccessMsg = new RichEmbed()
+                        .setTitle(`Prefix changed to ${args[1]}`)
+                        .setColor(embedColor)
+                        return message.channel.send(prefixSuccessMsg)
+
+                    }else if (reaction.emoji.name === '👎') {
+                        const cancelMsg = new RichEmbed()
+                            .setTitle("Prefix change cancelled")
+                            .setColor(embedColor)
+                        return message.channel.send(cancelMsg)
+
+                    }else{
+                        message.reply('That is not a valid reaction')
+                    }
+
+                })
+                .catch(() => {
+                    const timeMsg = new RichEmbed()
+                        .setTitle('You did not respond in time')
+                        .setColor(embedColor)
+                    return message.channel.send(timeMsg)
+                })
+        }else if(!args[1]){
+            message.reply('What are you trying to set the prefix to?')
+        }
+    }
 }
 client.on('message', message => {
     
     
-    const args = message.content.slice(prefix.length).split(' ');
+    const args = message.content.slice(config.prefix.length).split(' ');
     const command = args.shift().toLowerCase();
-    
-    if (command === 'd'){
-        dieCommand(message, args)
-    }
-    else if (command === `poke`) {
-        pokeCommand(message, args)
-    }
-    else if(command === 'help'){
-        helpCommand(message)
+    if(message.content.startsWith(`${config.prefix}`)){
+        if (command === 'd'){
+            dieCommand(message, args)
+        }
+        else if (command === `poke`) {
+            pokeCommand(message, args)
+        }
+        else if(command === 'help'){
+            helpCommand(message)
+        }
+        else if(command === "prefix"){
+            prefixCommand(message, args)
+        }
     }
     
 
